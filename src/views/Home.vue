cac525e93f124bd2f9adc08f3c6a64afbbaa8e89
chrysthlyn <chrysthlyn@gmail.com>
2021-10-30 00:48:19 +0700

    Upload assets and add component header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderSheina />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSheina from "../components/HeaderSheina.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HeaderSheina,
+  },
+};
 </script>
