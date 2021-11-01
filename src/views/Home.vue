f2ea060473bbd5706d5491c6be84ca0207d60082
chrysthlyn <chrysthlyn@gmail.com>
2021-11-01 16:33:25 +0700

    Add component banner section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <HeaderSheina />
     <HeroSheina />
+    <BannerSheina />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import HeaderSheina from "../components/HeaderSheina.vue";
 import HeroSheina from "../components/HeroSheina.vue";
+import BannerSheina from "../components/BannerSheina.vue";
 
 export default {
   name: "Home",
   components: {
     HeaderSheina,
     HeroSheina,
+    BannerSheina,
   },
 };
 </script>
