ee39b5787c33e5380d6f0cf326af379db02b7f5c
chrysthlyn <chrysthlyn@gmail.com>
2021-11-02 21:43:16 +0700

    Add new page product

---
+++ src/views/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="product">
+    <HeaderSheina />
+    <FooterSheina />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderSheina from "../components/HeaderSheina.vue";
+import FooterSheina from "../components/FooterSheina.vue";
+
+export default {
+  name: "Product",
+  components: {
+    HeaderSheina,
+    FooterSheina,
+  },
+};
+</script>
