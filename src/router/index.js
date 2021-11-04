2f68c4567e13866c54916d10c583b6d323857bd3
chrysthlyn <chrysthlyn@gmail.com>
2021-11-04 14:03:02 +0700

    Add new page success checkout

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Product from "../views/Product.vue";
 import ShoppingCart from "../views/ShoppingCart.vue";
+import Success from "../views/Success.vue";
 
 Vue.use(VueRouter);
 
@@ -30,6 +31,11 @@ const routes = [
     path: "/cart",
     name: "cart",
     component: ShoppingCart,
+  },
+  {
+    path: "/success",
+    name: "Success",
+    component: Success,
   },
 ];
 
