9b6e48c75080c292a3bad264c3571bf2c5b4526b
chrysthlyn <chrysthlyn@gmail.com>
2021-11-04 13:51:13 +0700

    Add new page shopping cart

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Product from "../views/Product.vue";
+import ShoppingCart from "../views/ShoppingCart.vue";
 
 Vue.use(VueRouter);
 
@@ -24,6 +25,11 @@ const routes = [
     path: "/product",
     name: "Product",
     component: Product,
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: ShoppingCart,
   },
 ];
 
